befc7a42dc7eb9f4e58947665546d9a0b5f6837f
ryn <120092610+rayhan889@users.noreply.github.com>
2025-05-05 11:58:29 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>NextTS Starter Project</h1>
+      </div>
+    </section>
+  )
+}
